1cf197283ec3f84969a6bca024af1f1c7a81e0da
Sagnick-Mondal <sagnickm.official@gmail.com>
2025-11-16 18:21:46 +0530

    bug fix #3

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-// @ts-expect-error: side-effect import of CSS without type declarations
 import "./globals.css";
 
 const geistSans = Geist({
